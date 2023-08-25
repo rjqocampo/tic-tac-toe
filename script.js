0c77597307d095a006cc3a8ca0d572eb3f0fdb9c
RJ Ocampo <rjqocampo@gmail.com>
2023-08-25 15:14:38 +0800

    Add functions to display grids

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const dialogPreRound = document.querySelector('.dialog-pre-round');
-const buttonPreRound = document.querySelector('.button-pre-round');
-
-buttonPreRound.addEventListener('click', () => dialogPreRound.close())
-
-dialogPreRound.showModal();
-
 /*
-Grid has 3 states: null, X, O;
+Grid has 3 states: 0, 1, 2;
 2 player objects. Has 2 states: X or O
 
 public function
@@ -17,3 +10,63 @@ Intro modal
 Choose mark: X or O
 Default players[0]: 
 */
+
+const gameModule = (function() {
+  const grids = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  const getGrids = () => grids;
+
+  return {
+    getGrids
+  }
+})();
+
+const displayModule = (function() {
+  const dialogPreRound = document.querySelector('.dialog-pre-round');
+  const buttonPreRound = document.querySelector('.button-pre-round');
+  const main = document.querySelector('main');
+
+  buttonPreRound.addEventListener('click', () => dialogPreRound.close())
+  dialogPreRound.showModal();
+
+  function showGrids() {
+    gameModule.getGrids().forEach((grid) => {
+      const div = document.createElement('div');
+      div.setAttribute('class', 'grid');
+      main.appendChild(div);
+    });
+  }
+
+  function removeGrids() {
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
+    }
+  }
+
+  showGrids();
+})();
+
+const flowModule = (function () {
+
+})();
+
+const playerModule = (function() {
+  const players = [
+    {
+      name: 'Player 1',
+      mark: 'X',
+    },
+    {
+      name: 'Player 2',
+      mark: 'O',
+    }
+  ];
+
+  function log() {
+    console.log(players);
+  }
+
+  return {
+    log
+  }
+})();
