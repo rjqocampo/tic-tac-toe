a574647313dbbe0ab64972c5db10b2a27c00eb23
RJ Ocampo <rjqocampo@gmail.com>
2023-08-25 14:02:05 +0800

    Add pre-round screen

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const dialogPreRound = document.querySelector('.dialog-pre-round');
+const buttonPreRound = document.querySelector('.button-pre-round');
+
+buttonPreRound.addEventListener('click', () => dialogPreRound.close())
+
+dialogPreRound.showModal();
+
+/*
+Grid has 3 states: null, X, O;
+2 player objects. Has 2 states: X or O
+
+public function
+set mark
+reset game
+
+Intro modal
+Choose mark: X or O
+Default players[0]: 
+*/
